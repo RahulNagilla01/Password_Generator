3b5082d9886cefebd6c93937c54b634683320ed9
okaGeek <rahulnagilla0@gmail.com>
2025-01-12 22:52:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+NumberOfPasswords = int(input("Please enter number of passwords you need"))
+LengthOfPassword = int(input("Please enter length of the password")
+
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-=_+[]\';./,<>?:\"{}|`~"                      
+for passwords in range(NumberOfPasswords):
+  for 
